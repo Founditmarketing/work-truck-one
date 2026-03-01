ee933c34450fa81f5ec79446ee6ca576496c19fe
Trevo <trevor@g3tree.com>
2026-03-01 12:54:40 -0600

    Fix React Router scroll restoration so links start at the top of the page

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import ServicePage from './pages/ServicePage';
+import ScrollToTop from './components/ScrollToTop';
 
 export default function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
